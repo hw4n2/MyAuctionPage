eb2273e87c471a5df15018f97a72c744ca6303f1
KJH <kjh557711@gmail.com>
2024-05-09 11:57:35 +0900

    loginform start

---
+++ public/script/signScript.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let signUpBtn = document.getElementById("signUp_btn");
+    signUpBtn.addEventListener("click", function () {
+        window.location.href = '/signUp';
+    });
+    
+});
--- public/script/totalScript.js
+++ public/script/totalScript.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
     let logoImg = document.getElementById("logoImg");
     logoImg.addEventListener("click", function () {
         window.location.href = '/';
     });
+
+    let loginBtn = document.getElementById("loginBtn");
+    loginBtn.addEventListener("click", function () {
+        window.location.href = '/signIn';
+    });
+
+    let signUpBtn = document.getElementById("signUpBtn");
+    signUpBtn.addEventListener("click", function () {
+        window.location.href = '/signUp';
+    });
     
 });
+
