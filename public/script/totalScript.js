895e5011b36a047e283dc24b7d1847cc2472bdd1
KJH <kjh557711@gmail.com>
2024-05-12 00:42:16 +0900

    beforeLoginlogic

--- public/script/totalScript.js
+++ public/script/totalScript.js
@@ -20,10 +20,12 @@ window.addEventListener("load", function () {
         window.location.href = '/signUp';
     });
 
-    let to_act_btn = document.getElementById("to_auction");
-    to_act_btn.addEventListener("click", function() {
-        window.location.href = '/curAuctions'
-    })
+    let to_act_btn = document.getElementsByClassName("navBtn");
+    for(let i = 0; i < 3; i++){
+        to_act_btn[i].addEventListener("click", function() {
+            window.location.href = '/curAuctions';
+        });
+    };
     
 });
 
