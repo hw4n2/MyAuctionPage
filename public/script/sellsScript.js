babe5f958bb96212c800f14511cc72428d0f9381
KJH <kjh557711@gmail.com>
2024-06-08 19:29:15 +0900

    bid alert message localestring

--- public/script/sellsScript.js
+++ public/script/sellsScript.js
@@ -4,7 +4,7 @@ function submitConfirm(){
     const expire_date = document.getElementById("expire_date").value;
     const expire_time = document.getElementById("expire_time").value;
     return confirm(`제품명 : ${name}
-가격 : ${price} 원
+가격 : ${parseInt(price).toLocaleString()} 원
 경매 종료일 : ${expire_date} ${expire_time}
 위 기한까지 최고 응찰가를 제시한 고객이 낙찰자로 선정됩니다.
 등록하시겠습니까?`
